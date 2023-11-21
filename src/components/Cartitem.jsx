89eb3735f1b12d850e5caab49dc85c8b77cd55ac
RajsekharPanda <rajsekharpandawork@gmail.com>
2023-11-21 17:54:22 +0530

    done with cart

--- src/components/Cartitem.jsx
+++ src/components/Cartitem.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
-export const Cartitem = () => {
+const Cartitem = () => {
   return (
     <div>Cartitem</div>
   )
 }
+
+
+export default Cartitem
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import Cartitem from '../components/Cartitem'
 
 const Cart = () => {
+
+
+  const {cart} = useSelector((state) => state);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect( () => {
+    setTotalAmount( cart.reduce( (acc, curr) => acc + curr.price,0));
+  },[cart])
+
   return (
-    <div>Cart</div>
+    <div>
+      {
+        cart.length > 0 ? 
+        (
+          <div>
+            <div>
+              {
+                cart.map( (item, index) => {
+                  return <Cartitem key={item.id} item={item} itemIndex={index}/>
+                })
+              }
+            </div>
+
+            <div>
+
+              <div>
+                <div>Your Cart</div>
+                <div>Summary</div>
+                <p>
+                  <span>Total Items: {cart.length}</span>
+                </p>
+              </div>
+
+              <div>
+                <p>Total Amount: ${totalAmount}</p>
+                <button>Check Out Now</button>
+              </div>
+
+            </div>
+            
+
+          </div>
+        ) :
+        (
+          <div>
+            <h2>Cart Empty</h2>
+            <link to={"/"}>
+              <button>
+                Shop Now
+              </button>
+            </link>
+          </div>
+        )
+      }
+    </div>
   )
 }
 
