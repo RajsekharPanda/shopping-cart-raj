adb01beb66f0ad7a2f18c00f4970682e6356eab9
RajsekharPanda <rajsekharpandawork@gmail.com>
2023-11-21 12:58:48 +0530

    .init and added the base of the project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ImCart } from "react-icons/im";
+
+
+export const Navbar = () => {
+  return (
+    <div>
+        <div className='flex flex-row justify-between'>
+            <img src="" alt="" />
+            <div>
+                <p>Home</p>
+                <ImCart />
+            </div>
+        </div>
+    </div>
+  )
+}
